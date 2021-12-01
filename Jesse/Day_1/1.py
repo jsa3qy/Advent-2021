1ae7e8b0228afadca0dc155ce8c1ea17c0a741a3
Jesse Alloy <jessealloy@jesses-air.myfiosgateway.com>
2021-12-01 08:11:32 -0500

    Jesse Day 1

---
+++ Jesse/Day_1/1.py
@@ -0,0 +1,21 @@
+
+def part_1():
+    lines = [int(line.strip()) for line in open("input.txt")]
+    increased = 0
+    for index,val in enumerate(lines):
+        if index > 0:
+            if val > lines[index - 1]:
+                increased += 1
+    return increased
+
+def part_2():
+    lines = [int(line.strip()) for line in open("input.txt")]
+    increased = 0
+    for index,val in enumerate(lines):
+        if index < len(lines) - 3:
+            if val + lines[index + 1] + lines[index + 2]  < lines[index + 1] + lines[index + 2] + lines[index + 3]:
+                increased += 1
+    return increased
+
+print("Part 1: " + str(part_1()))
+print("Part 2: " + str(part_2()))
